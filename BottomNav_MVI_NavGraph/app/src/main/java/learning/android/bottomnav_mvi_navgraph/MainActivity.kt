9b4a9161133d3cf07486ca4372e0a48d202c6578
ATH\vtsapalo <Vasilios.TSAPALOS@intrasoft-intl.com>
2020-11-18 12:24:16 +0200

    Add Main Activity

---
+++ BottomNav_MVI_NavGraph/app/src/main/java/learning/android/bottomnav_mvi_navgraph/MainActivity.kt
@@ -0,0 +1,11 @@
+package learning.android.bottomnav_mvi_navgraph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
