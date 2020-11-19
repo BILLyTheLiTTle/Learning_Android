0c50cbcdbe262eaf308dbd363b07deb63e4333d9
ATH\vtsapalo <Vasilios.TSAPALOS@intrasoft-intl.com>
2020-11-19 13:46:15 +0200

    Simple back button functionality

--- BottomNav_MVI_NavGraph/app/src/main/java/learning/android/bottomnav_mvi_navgraph/ui/MainActivity.kt
+++ BottomNav_MVI_NavGraph/app/src/main/java/learning/android/bottomnav_mvi_navgraph/ui/MainActivity.kt
@@ -2,13 +2,12 @@ package learning.android.bottomnav_mvi_navgraph.ui
 
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import learning.android.bottomnav_mvi_navgraph.R
 import learning.android.bottomnav_mvi_navgraph.ui.fragments.FirstFragment
@@ -19,21 +18,25 @@ import learning.android.bottomnav_mvi_navgraph.ui.fragments.ThirdFragment
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private val model by viewModels<MyViewModel>()
+    private lateinit var navigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         navigation.setOnNavigationItemSelectedListener(this)
 
         loadFragment(FirstFragment())
     }
 
-    private fun loadFragment(fragment: Fragment) {
+    private fun loadFragment(fragment: Fragment, addToBackStack: Boolean = false) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
-        transaction.addToBackStack(null)
+        if (addToBackStack) {
+            transaction.addToBackStack(null)
+        }
+        supportFragmentManager.popBackStack()
         transaction.commit()
     }
 
@@ -58,5 +61,18 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         }
         loadFragment(fragment)
         return true
+    }
+
+    /*
+    Solution found here: https://stackoverflow.com/questions/43870485/how-to-handle-bottom-navigation-perfectly-with-back-pressed
+    Don't add to backstack
+     */
+    override fun onBackPressed() {
+        val selectedItemId = navigation.selectedItemId
+        if (R.id.first_page_menu_item == selectedItemId) {
+            super.onBackPressed()
+        } else {
+            navigation.selectedItemId = R.id.first_page_menu_item
+        }
     }
 }
