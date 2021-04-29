5712e838498d8d1d3e89c40985ba98a18b93e6df
ATH\vtsapalo <Vasilios.TSAPALOS@intrasoft-intl.com>
2021-04-29 15:03:02 +0300

    Implement different UI for advanced flavor

--- Hilt_Example/app/src/advanced/java/learning/android/hilt/viewmodel/MainActivityViewModel.kt
+++ Hilt_Example/app/src/advanced/java/learning/android/hilt/viewmodel/MainActivityViewModel.kt
@@ -1,5 +1,8 @@
 package learning.android.hilt.viewmodel
 
+import androidx.databinding.Observable
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,17 +13,43 @@ import learning.android.domain.models.response.TodayWeatherModel
 import learning.android.domain.usecases.GetTodayWeatherUseCase
 import javax.inject.Inject
 
+
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val todayWeatherUseCase: GetTodayWeatherUseCase
 ) : ViewModel() {
+
+    @Inject
+    lateinit var city: City
+
+    val spinnerCitySelections = ObservableArrayList<String>()
+    val selectedCityPosition = ObservableField<Int>()
 
     private val _todayWeather = MutableLiveData<TodayWeatherModel>()
     fun getTodayWeatherLiveData(): LiveData<TodayWeatherModel> {
         return _todayWeather
     }
 
-    fun getTodayWeather(city: City) {
+    init {
+        spinnerCitySelections.addAll(arrayListOf("Patras", "Athens"))
+        selectedCityPosition.onPropertyChangedCallback {
+            city.name = spinnerCitySelections[it ?: 0]
+            getTodayWeather(city)
+        }
+    }
+
+    private fun <T> ObservableField<T>.onPropertyChangedCallback(
+        action: (item: T?) -> Unit
+    ) {
+        addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(observable: Observable, i: Int) {
+                action(get())
+            }
+        })
+    }
+
+    private fun getTodayWeather(city: City) {
         todayWeatherUseCase.city = city
         todayWeatherUseCase.execute()
             .subscribeOn(Schedulers.io())
