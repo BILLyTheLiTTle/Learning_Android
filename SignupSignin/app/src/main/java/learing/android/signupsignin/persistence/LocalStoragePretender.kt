0604f4469aab0c419b7c06bc58c4495f18060334
ATH\vtsapalo <Vasilios.TSAPALOS@intrasoft-intl.com>
2022-04-01 17:41:29 +0300

    Create basic UI

---
+++ SignupSignin/app/src/main/java/learing/android/signupsignin/persistence/LocalStoragePretender.kt
@@ -0,0 +1,7 @@
+package learing.android.signupsignin.persistence
+
+/**
+ * Pretends to be  a local storage class. It stores but not in the local storage, it stores in variables!!!
+ */
+object LocalStoragePretender {
+}
---
+++ SignupSignin/app/src/main/java/learing/android/signupsignin/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package learing.android.signupsignin.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import learing.android.signupsignin.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
