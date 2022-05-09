36aab38f1b2c4697306858cefc5c897ccc97abf9
v.tsapalos <v.tsapalos@kaizengaming.com>
2022-05-09 16:31:48 +0300

    Project setup and dependencies implementation

--- Gradle_Example/Hilt_Example/app/src/advanced/java/learning/android/hilt/ui/MainActivity.kt
+++ Gradle_Example/Hilt_Example/app/src/advanced/java/learning/android/hilt/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.skydoves.balloon.*
 import dagger.hilt.android.AndroidEntryPoint
 import learning.android.hilt.R
 import learning.android.hilt.databinding.ActivityMainBinding
@@ -22,6 +23,22 @@ class MainActivity : AppCompatActivity() {
         )
 
         layout.viewModel = viewModel
+
+        val balloon = createBalloon(this) {
+            setWidthRatio(1.0f)
+            setHeight(BalloonSizeSpec.WRAP)
+            setText("Edit your profile here!")
+            setTextSize(15f)
+            setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+            setArrowSize(10)
+            setArrowPosition(0.5f)
+            setPadding(12)
+            setCornerRadius(8f)
+            setBalloonAnimation(BalloonAnimation.ELASTIC)
+            setLifecycleOwner(lifecycleOwner)
+            build()
+        }
+        layout.descriptionTextview.showAlignTop(balloon, 5, 5)
 
         viewModel.getTodayWeatherLiveData().observe(this) {
             layout.temperatureValueTextview.text = it.temperature
--- Gradle_Example/Hilt_Example/app/src/simple/java/learning/android/hilt/ui/MainActivity.kt
+++ Gradle_Example/Hilt_Example/app/src/simple/java/learning/android/hilt/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import com.skydoves.balloon.*
 import dagger.hilt.android.AndroidEntryPoint
 import learning.android.domain.models.request.City
 import learning.android.hilt.R
@@ -32,5 +33,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.getTodayWeather(city)
+
+        val balloon = createBalloon(this) {
+            setWidthRatio(1.0f)
+            setHeight(BalloonSizeSpec.WRAP)
+            setText("Edit your profile here!")
+            setTextSize(15f)
+            setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+            setArrowSize(10)
+            setArrowPosition(0.5f)
+            setPadding(12)
+            setCornerRadius(8f)
+            setBalloonAnimation(BalloonAnimation.ELASTIC)
+            setLifecycleOwner(lifecycleOwner)
+            build()
+        }
+        layout.descriptionTextview.showAlignTop(balloon, 5, 5)
     }
 }
