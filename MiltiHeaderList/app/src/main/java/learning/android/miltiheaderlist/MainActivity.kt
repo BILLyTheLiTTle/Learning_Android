40cf9fb92081ba1839d8f6e052f55ed197f23788
v.tsapalos <v.tsapalos@kaizengaming.com>
2022-05-16 18:25:06 +0300

    Improve Indexed LazyColumn implementation

--- MiltiHeaderList/app/src/main/java/learning/android/miltiheaderlist/MainActivity.kt
+++ MiltiHeaderList/app/src/main/java/learning/android/miltiheaderlist/MainActivity.kt
@@ -28,8 +28,7 @@ import androidx.compose.ui.unit.sp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
-import learning.android.miltiheaderlist.library.DoubleHeaderLazyColumn
-import learning.android.miltiheaderlist.library.IndexColumn
+import learning.android.miltiheaderlist.library.*
 import learning.android.miltiheaderlist.ui.theme.MiltiHeaderListTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,7 +42,8 @@ class MainActivity : ComponentActivity() {
                 Surface(color = MaterialTheme.colors.background) {
                     //MainList(getTheData())
 //                    ExampleDoubleHeaderList(getTheData())
-                    ExampleIndexedList(getTheIndexedData())
+                    ExampleIndexedListWithCustomLazyColumn(getTheIndexedData())
+//                    ExampleIndexedListWithData(getTheIndexedData())
                 }
             }
         }
@@ -53,11 +53,11 @@ class MainActivity : ComponentActivity() {
 @ExperimentalFoundationApi
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
-fun ExampleIndexedList(data: List<CustomListItem2>,
+fun ExampleIndexedListWithCustomLazyColumn(data: List<CustomListItem2>,
                        indices: List<Char> = ('A'..'Z').toList()) {
     val lazyListState = rememberLazyListState()
 
-    IndexColumn(
+    IndexColumnWithCustomLazyColumn(
         // The list of the indices
         indices = indices,
         // The state of the main LazyColumn, the one with the real data
@@ -70,7 +70,7 @@ fun ExampleIndexedList(data: List<CustomListItem2>,
         predicate = {
             data.indexOfFirst { item ->
                 item.surname.startsWith(it.toString(), true) }
-            },
+                    },
         // The list of the main data
         lazyColumnContent = {
             LazyColumn(state = lazyListState) {
@@ -95,10 +95,66 @@ fun ExampleIndexedList(data: List<CustomListItem2>,
             }
         },
         // The item content for the indices list
-        indexedItemContent = {
-            Text(text = it.toString(), fontSize = 20.sp)
+        indexedItemContent = { item, isSelected ->
+            Text(
+                modifier = Modifier
+                    .background(color = Color.Transparent),
+                color = if (isSelected) Color.Blue else Color.Black,
+                text = item.toString(),
+                fontSize = 20.sp)
         }
- )
+    )
+}
+
+@ExperimentalFoundationApi
+@RequiresApi(Build.VERSION_CODES.N)
+@Composable
+fun ExampleIndexedListWithData(data: List<CustomListItem2>,
+                       indices: List<Char> = ('A'..'Z').toList()) {
+
+    IndexColumnWithData(
+        // The list of the indices
+        indices = indices,
+        // The list of the actual data
+        data = data,
+        // The modifier is exported for the Column, the one with the indices
+        modifier = Modifier
+            .background(color = Color.Transparent)
+            .height(300.dp),
+        // The way to connect the index with a data item (here the index item matches the first letter of the surname)
+        predicate = {
+            data.indexOfFirst { item ->
+                item.surname.startsWith(it.toString(), true) }
+        },
+        // The list of the main data
+        mainItemContent = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+                    .clickable { },
+                elevation = 10.dp
+            ) {
+                Column {
+                    Text(text = data[it].surname,
+                        fontSize = 17.sp, fontWeight = FontWeight.Bold, fontStyle = FontStyle.Italic,
+                        modifier = Modifier.padding(start = 10.dp))
+                    Text(text = data[it].name,
+                        fontSize = 15.sp, fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(start = 15.dp))
+                }
+            }
+        },
+        // The item content for the indices list
+        indexedItemContent = { item, isSelected ->
+            Text(
+                modifier = Modifier
+                    .background(color = Color.Transparent),
+                color = if (isSelected) Color.Blue else Color.Black,
+                text = item.toString(),
+                fontSize = 20.sp)
+        }
+    )
 }
 
 @ExperimentalCoilApi
--- MiltiHeaderList/app/src/main/java/learning/android/miltiheaderlist/library/IndexColumn.kt
+++ MiltiHeaderList/app/src/main/java/learning/android/miltiheaderlist/library/IndexColumn.kt
@@ -7,52 +7,158 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.lazy.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+
 
 @RequiresApi(Build.VERSION_CODES.N)
 @ExperimentalFoundationApi
 @Composable
-fun <T> IndexColumn(
+fun <T> IndexColumnWithCustomLazyColumn(
     indices: List<T>,
     itemsListState: LazyListState,
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     predicate: (T) -> Int,
     lazyColumnContent: @Composable () -> Unit,
-    indexedItemContent: @Composable (T) -> Unit
+    indexedItemContent: @Composable (T, Boolean) -> Unit
 ) {
     val coroutineContext = rememberCoroutineScope()
+    val indexState = rememberLazyListState()
+    val shouldUpdateSelection = remember {
+        mutableStateOf(false)
+    }
+    val selectedIndex = remember {
+        mutableStateOf(0)
+    }
 
     Box(contentAlignment = Alignment.CenterEnd) {
         Column() {
             lazyColumnContent()
         }
-        Column(
+
+        LazyColumn(
+            state = indexState,
             horizontalAlignment = Alignment.End,
             modifier = modifier
-                .verticalScroll(rememberScrollState())
         ) {
-            indices.forEach { item ->
+            if (!indexState.isScrollInProgress && shouldUpdateSelection.value) {
+                val list = indexState.layoutInfo.visibleItemsInfo
+                val startIndex = list.first().index
+
+                scrollListBasedOnIndex(
+                    coroutineContext, predicate,
+                    indices, itemsListState,
+                    selectedIndex, startIndex
+                )
+
+                shouldUpdateSelection.value = false
+            } else if (indexState.isScrollInProgress) {
+                shouldUpdateSelection.value = true
+            }
+
+            itemsIndexed(indices) { index, item ->
                 Box(modifier = Modifier
                     .padding(5.dp)
                     .clickable {
-                        coroutineContext.launch {
-                            val itemIndex = predicate(item)
-                            if (itemIndex >= 0) {
-                                itemsListState.scrollToItem(itemIndex)
-                            }
-                        }
+                        scrollListBasedOnIndex(
+                            coroutineContext, predicate,
+                            indices, itemsListState,
+                            selectedIndex, index
+                        )
                     }) {
-                    indexedItemContent(item)
+                    indexedItemContent(item, index == selectedIndex.value)
                 }
             }
         }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.N)
+@ExperimentalFoundationApi
+@Composable
+fun <T> IndexColumnWithData(
+    indices: List<T>,
+    data: List<T>,
+    modifier: Modifier = Modifier,
+    predicate: (T) -> Int,
+    mainItemContent: @Composable (Int) -> Unit,
+    indexedItemContent: @Composable (T, Boolean) -> Unit
+) {
+    val coroutineContext = rememberCoroutineScope()
+    val indexState = rememberLazyListState()
+    val itemsState = rememberLazyListState()
+    val shouldUpdateSelection = remember {
+        mutableStateOf(false)
+    }
+    val selectedIndex = remember {
+        mutableStateOf(0)
+    }
+
+    Box(contentAlignment = Alignment.CenterEnd) {
+        Column() {
+            LazyColumn(state = itemsState) {
+                itemsIndexed(data) { index, item ->
+                    mainItemContent(index)
+                }
+            }
+        }
+
+        LazyColumn(
+            state = indexState,
+            horizontalAlignment = Alignment.End,
+            modifier = modifier
+        ) {
+            if (!indexState.isScrollInProgress && shouldUpdateSelection.value) {
+                val list = indexState.layoutInfo.visibleItemsInfo
+                val startIndex = list.first().index
+
+                scrollListBasedOnIndex(
+                    coroutineContext, predicate,
+                    indices, itemsState,
+                    selectedIndex, startIndex
+                )
+
+                shouldUpdateSelection.value = false
+            } else if (indexState.isScrollInProgress) {
+                shouldUpdateSelection.value = true
+            }
+
+            itemsIndexed(indices) { index, item ->
+                Box(modifier = Modifier
+                    .padding(5.dp)
+                    .clickable {
+                        scrollListBasedOnIndex(
+                            coroutineContext, predicate,
+                            indices, itemsState,
+                            selectedIndex, index
+                        )
+                    }) {
+                    indexedItemContent(item, index == selectedIndex.value)
+                }
+            }
+        }
+    }
+}
+
+private fun <T> scrollListBasedOnIndex(
+    coroutineContext: CoroutineScope,
+    predicate: (T) -> Int,
+    indices: List<T>,
+    itemsListState: LazyListState,
+    selectedIndex: MutableState<Int>,
+    index: Int
+) {
+    coroutineContext.launch {
+        val itemIndex = predicate(indices[index])
+        if (itemIndex >= 0) {
+            itemsListState.scrollToItem(itemIndex)
+        }
+        selectedIndex.value = index
     }
 }
